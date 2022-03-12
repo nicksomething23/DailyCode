6614fda50470dd94914bf378abc3665250ccc2b1
Nicolas Peralta <nicolasperalta27@gmail.com>
2022-03-12 16:54:55 -0500

    add code

--- fundamentals.js
+++ fundamentals.js
@@ -31,7 +31,13 @@ function bmi(weight, height) {
 *************************************
 */
 
-
+function SeriesSum(n) {
+    let sum = 0
+    for (let i = 0; i < n; i++) {
+      sum += 1 / (i*3 + 1)
+    }
+    return sum.toFixed(2)
+  }
 
 /*
 ************************************
