a245ff9a3831ae589a726e2772e09d197874a21f
Nicolas Peralta <nicolasperalta27@gmail.com>
2022-04-05 23:22:00 -0400

    update javascript

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
-const rps = (p1, p2) => {
-    if ((p1 === 'paper' && p2 ==='scissors') || (p1 === 'scissors' && p2 ==='rock') || (p1 === 'rock' && p2 ==='paper')) {
-      return "Player 2 won!"
-    } else if ((p2 === 'paper' && p1 ==='scissors') || (p2 === 'scissors' && p1 ==='rock') || (p2 === 'rock' && p1 ==='paper')) {
-      return "Player 1 won!"
-    } else {
-      return "Draw!"
-    }
-  };
+// const rps = (p1, p2) => {
+//     if ((p1 === 'paper' && p2 ==='scissors') || (p1 === 'scissors' && p2 ==='rock') || (p1 === 'rock' && p2 ==='paper')) {
+//       return "Player 2 won!"
+//     } else if ((p2 === 'paper' && p1 ==='scissors') || (p2 === 'scissors' && p1 ==='rock') || (p2 === 'rock' && p1 ==='paper')) {
+//       return "Player 1 won!"
+//     } else {
+//       return "Draw!"
+//     }
+//   };
+
+function sum (numbers) {
+  "use strict";
+if (numbers.length === 0) {
+return 0;
+} else {
+return numbers.reduce((a,b) => a += b ,0)
+}
+
+
+};
