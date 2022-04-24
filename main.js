2af4c94dd17e61786ead2d00362ccb2b64c2d238
Nicolas Peralta <nicolasperalta27@gmail.com>
2022-04-24 19:55:05 -0400

    add js

--- main.js
+++ main.js
@@ -201,6 +201,21 @@
 //     return str.split(' ').reverse().join(" ");
 //   }
 
+// function magNumber(info){
+  
+//     if (info[0] === "PT92") {
+//       return Math.ceil(info[1] * 3 / 17)
+//     } else if (info[0] === "M4A1") {
+//        return Math.ceil(info[1] * 3 / 30)
+//     } else if (info[0] === "M16A2") {
+//        return Math.ceil(info[1] * 3 / 30)
+//     } else if (info[0] === "PSG1") {
+//        return Math.ceil(info[1] * 3 / 5)
+//     }
+    
+//   }
+  
+
 
 // /* 7 kyu problems*/
 
