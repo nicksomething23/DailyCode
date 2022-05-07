d35b01deb177b5793cb97f66e0a1c00a5eac1ead
Nicolas Peralta <nicolasperalta27@gmail.com>
2022-05-06 23:59:57 -0400

    add example

--- main.js
+++ main.js
@@ -302,7 +302,10 @@
 //   }
 
 
-
+// function warnTheSheep(queue) {
+//     const position = queue.reverse().indexOf('wolf');
+//     return position === 0 ? 'Pls go away and stop eating my sheep' : `Oi! Sheep number ${ position }! You are about to be eaten by a wolf!`;
+//   }
 
 
 // /* 7 kyu problems*/
