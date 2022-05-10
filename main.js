a15dbabdc26cea008432d7cce75d274c79ab1309
Nicolas Peralta <nicolasperalta27@gmail.com>
2022-05-09 22:27:34 -0400

    add code

--- main.js
+++ main.js
@@ -375,6 +375,20 @@
 //     return array.join(',')
 //   }
 
+// const quarterOf = (month) => {
+//     if (month <= 3) {
+//       return 1
+//     } else if (month <= 6) {
+//       return 2
+//     } else if (month <= 9) {
+//       return 3
+//     } else {
+//       return 4
+//     }
+    
+//   }
+
+
 // function monkeyCount(n) {
 //     let arr = []
 //     for (let i = 1; i <= n;i++ ) {
