1634fb9bf3d3dae9b5d780a3459c4cf7ed8b5600
Nicolas Peralta <nicolasperalta27@gmail.com>
2022-06-19 20:17:40 -0400

    update array

--- array.js
+++ array.js
@@ -452,6 +452,12 @@ function invert(array) {
 //     } 
 //   }
 
+const reverseSeq = n => {
+  let arr = [];
+    for (let i=n; i>0; i--) {
+      arr.push(i);
+      } return arr;
+  };
 
 /*
 ************************************
