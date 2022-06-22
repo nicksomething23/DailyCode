5d63ca7165cc20ea0459709e4f8fe2e0f1b3dcd0
Nicolas Peralta <nicolasperalta27@gmail.com>
2022-06-21 20:23:42 -0400

    update fundamentals

--- fundamentals.js
+++ fundamentals.js
@@ -8,6 +8,21 @@
 *************************************
 */
 
+function basicOp(operation, value1, value2) {
+  switch (operation) {
+      case '+':
+          return value1 + value2;
+      case '-':
+          return value1 - value2;
+      case '*':
+          return value1 * value2;
+      case '/':
+          return value1 / value2;
+      default:
+          return 0;
+  }
+}
+
 const greet = () => `hello world!`
 
 class SmallestIntegerFinder {
