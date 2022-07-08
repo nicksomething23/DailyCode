80b24af647732053d68641f29246a0702eb37824
Nicolas Peralta <nicolasperalta27@gmail.com>
2022-07-07 21:51:54 -0400

    update fundamentals

--- fundamentals.js
+++ fundamentals.js
@@ -8,6 +8,16 @@
 *************************************
 */
 
+function lovefunc(flower1, flower2){
+  if ((flower1 % 2 == 0) && (flower2 % 2 == 0)) {
+    return false
+  } else if ((flower1 % 2 !== 0) && (flower2 % 2 !== 0)) {
+    return false
+  } else {
+    return true
+  }
+}
+
 // let laLigaGoals = 43;
 // let championsLeagueGoals = 10;
 // let copaDelReyGoals = 5;
